d741ac1cdcd78c536a5a994bbc3bffde6696e1a9
Saptarshi Chatterjee <saptarshichatterjee1@gmail.com>
2016-02-22 19:39:36 +0530

    Incorporate dans suggestion

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,15 +1,6 @@
 /*    eslint indent: 0 */
-import { createStore, compose } from 'redux';
-import { reduxReactRouter } from 'redux-router';
-import { createHashHistory as createHistory } from 'history';
-import routes from '../Routes';
+import { createStore } from 'redux';
 import rootReducer from '../reducers/rootReducer';
 
-const finalCreateStore = compose(
-    reduxReactRouter({
-        routes,
-        createHistory
-    }),
-)(createStore);
-const store = finalCreateStore(rootReducer);
+const store = createStore(rootReducer);
 export default store;
--- src/main.js
+++ src/main.js
@@ -2,13 +2,15 @@ import 'babel-core/polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { ReduxRouter } from 'redux-router';
 import store from './lib/store';
+import IndexPage from './components/indexPage';
 
 render(
     <div>
       <Provider store={store}>
-        <ReduxRouter />
+        <div>
+    		<IndexPage/>
+  		</div>
       </Provider>
     </div>,
     document.getElementById('content')
